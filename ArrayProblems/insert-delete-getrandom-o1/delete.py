3070a820ec0bcb011dce2ed4d66c8596623e7d63
Emtiaz Ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2026-02-17 04:33:23 +0600

    feat:add

---
+++ ArrayProblems/insert-delete-getrandom-o1/delete.py
@@ -0,0 +1,23 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.ran=set()
+
+    def insert(self, val: int) -> bool:
+        if val not in self.ran:
+            self.ran.add(val)
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        if val in self.ran:
+            self.ran.remove(val)
+            return True
+        return False
+        
+
+    def getRandom(self) -> int:
+        return random.choice(list(self.ran))
+        
+
+
